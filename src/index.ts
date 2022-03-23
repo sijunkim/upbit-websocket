c1b4030f73ea091dd2101d0864d3be4063559650
kimsijun <papaya9349@naver.com>
2022-03-23 17:03:23 +0900

    페어 추가, 반환 타입 설정, 함수 기능 분리

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,36 @@
-import { UpbitWebSocket } from "./websocket/upbit";
+import { UpbitWebSocket, UpbitWebSocketSimpleResponse } from "./websocket/upbit";
+
+const getRefindData = (data: UpbitWebSocketSimpleResponse) => {
+  
+  //안에 있는 데이터 정렬
+  /*
+  1. data.tms 날짜로 변경
+  2. orderbook: { asks : [{ap, as}], bids : [{bp, bs}] } 형식으로 데이터 구조 변경
+  3. krw-btc -> krw-eth 등 5개의 페어가 나오도록 처리
+  */
+  return null;
+}
+
+const printOrderbooks = (data: any) => {
+  //프린트 하는 로직 추가
+}
 
 async function Init() {
   try {
     const upbit_ws = new UpbitWebSocket();
 
-    upbit_ws.Open({ type: "orderbook", codes: ["KRW-BTC"] }, () => {
-      console.log("upbit socket connected.");
+    upbit_ws.Open({ type: "orderbook", codes: ["KRW-BTC","KRW-ETH","BTC-ETC","BTC-XRP","BTC-DOGE"] }, () => {
+      console.log("Wow upbit websocket connected.");
     });
 
     upbit_ws.OnMessage((data) => {
       console.log(data);
+      // const refinedData = getRefindData(data);
+      // printOrderbooks(refinedData);
     });
 
     upbit_ws.OnClose(() => {
-      console.log("closed.");
+      console.log("Oops upbit websocket closed.");
     });
   } catch (err) {
     console.log(`[ERROR]`, err);
--- src/websocket/upbit.ts
+++ src/websocket/upbit.ts
@@ -4,46 +4,14 @@ import WebSocket = require("ws");
 export interface UpbitSocketPayload {
   type: "ticker" | "trade" | "orderbook";
   codes: string[];
-  isOnlySnapshot?: boolean;
-  isOnlyRealtime?: boolean;
 }
 
 export interface UpbitWebSocketSimpleResponse {
   ty: string;
   cd: string;
-  op: number;
-  hp: number;
-  lp: number;
-  tp: number;
-  pcp: number;
-  atp: number;
-  c: string;
-  cp: number;
-  scp: number;
-  cr: number;
-  scr: number;
-  ab: string;
-  tv: number;
-  atv: number;
-  tdt: string;
-  ttm: string;
-  ttms: number;
-  aav: number;
-  abv: number;
-  h52wp: number;
-  h52wdt: string;
-  l52wp: number;
-  l52wdt: string;
-  ts: null;
-  ms: string;
-  msfi: null;
-  its: false;
-  dd: null;
-  mw: string;
   tms: number;
-  atp24h: number;
-  atv24h: number;
   st: string;
+  obu: string[];
 }
 
 export class UpbitWebSocket {
@@ -53,15 +21,20 @@ export class UpbitWebSocket {
     this.webSocket = new WebSocket("wss://api.upbit.com/websocket/v1");
   }
 
-  public Open(type: UpbitSocketPayload, callback?: Function) {
+  public Open(payload: UpbitSocketPayload, callback?: Function) {
     this.webSocket.onopen = () => {
-      //const formatString = `${JSON.stringify([{ "ticket": ticket() }, { "type": type }, { "format": "SIMPLE" }])}`;
-      const formatString = JSON.stringify([{"ticket":"test"},{"type":"trade","codes":["KRW-BTC","BTC-BCH"]},{"format":"SIMPLE"}]);
+      const formatString = JSON.stringify([{ ticket: ticket() }, payload, { format: "SIMPLE" } ]);
       this.webSocket.send(formatString);
-
       if (callback) {
         callback();
       }
+    };
+  }
+
+  public OnMessage(callback: (data: UpbitWebSocketSimpleResponse) => void) {
+    this.webSocket.onmessage = async (response) => {
+      const data = JSON.parse(response.data.toString("utf-8"));
+      callback(data);
     };
   }
 
@@ -72,13 +45,6 @@ export class UpbitWebSocket {
   public OnClose(callback: Function) {
     this.webSocket.onclose = () => {
       callback();
-    };
-  }
-
-  public OnMessage(callback: (data: UpbitWebSocketSimpleResponse) => void) {
-    this.webSocket.onmessage = async (response) => {
-      const data = JSON.parse(response.data.toString("utf-8"));
-      callback(data);
     };
   }
 }
